dee3bea70aa55cf7b92180c9b509fec306536d8e
xehdan <xehdan@yahoo.de>
2022-11-04 07:13:21 +0100

    Updated Form for CreateCustomer.jsx

--- src/components/customer/CreateCustomer.jsx
+++ src/components/customer/CreateCustomer.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
-import {Box, FormControlLabel, MenuItem, Radio, RadioGroup, TextField} from "@mui/material";
+import React, {useState} from 'react';
+import {
+    Card,
+    CardContent,
+    FormControlLabel,
+    Grid,
+    MenuItem,
+    Radio,
+    RadioGroup,
+    Stack,
+    TextField,
+    Typography
+} from "@mui/material";
 
 function CreateCustomer(props) {
     const addresses = [
@@ -12,34 +23,124 @@ function CreateCustomer(props) {
             label: 'Madam'
         }
     ]
+    // eslint-disable-next-line
+    const [custType, setCustType] = useState('');
+
 
 
     return (
-        <div>
-            <h1>Create Customer</h1>
-            <Box
-                component="form"
-                autocomplete="off"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                >
-                <RadioGroup
-                    row
-                    aria-labelledby="demo-row-radio-buttons-group-label"
-                    name="row-radio-buttons-group"
-                >
-                    <FormControlLabel value="B2C" control={<Radio />} label="B2C" />
-                    <FormControlLabel value="B2B" control={<Radio />} label="B2B" />
-                </RadioGroup>
-                <TextField label="Company Name" variant="standard"/>
-                <TextField label="First Name" variant="standard"/>
-                <TextField label="Last Name" variant="standard"/>
-                <TextField label="Address" select value={addresses}>{addresses.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>
-                ))}</TextField>
-            </Box>
-        </div>
+            <Grid container spacing={2} sx={{ margin: 3}}
+                  component="form"
+                  autoComplete="off">
+                <Grid item xs={12} xl={6}>
+                    <Card>
+                        <CardContent>
+
+                            <Stack>
+                                <RadioGroup
+                                    row
+                                    aria-labelledby="demo-row-radio-buttons-group-label"
+                                    name="row-radio-buttons-group"
+                                    required
+                                >
+                                    <FormControlLabel value="B2C" control={<Radio />} label="B2C" />
+                                    <FormControlLabel value="B2B" control={<Radio />} label="B2B" />
+                                </RadioGroup>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} xl={6}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h4" component="h2" gutterBottom>1. Personal Info</Typography>
+                            <Stack spacing={2}>
+                                <TextField label="Address" select>{addresses.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>
+                                ))}</TextField>
+                                <TextField variant="outlined" label="Company Name" />
+                                <TextField variant="outlined" label="VAT-ID" />
+                                <TextField variant="outlined" label="First Name" />
+                                <TextField variant="outlined" label="Last Name" />
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} xl={6}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h4" component="h2" gutterBottom>2. Delivery</Typography>
+                            <Stack spacing={2}>
+                                <Grid container>
+                                    <Grid item xs={12} md={9}>
+                                        <TextField fullWidth variant="outlined" required label="Street"  />
+                                    </Grid>
+                                    <Grid item xs={12} md={3}>
+                                        <TextField fullWidth variant="outlined" required label="Housenumber"/>
+                                    </Grid>
+                                </Grid>
+                                <TextField variant="outlined" label="Floor" />
+                                <Grid container>
+                                    <Grid item xs={12} md={3}>
+                                        <TextField fullWidth variant="outlined" required label="Zipcode"  />
+                                    </Grid>
+                                    <Grid item xs={12} md={9}>
+                                        <TextField fullWidth variant="outlined" required label="City"/>
+                                    </Grid>
+                                </Grid>
+                                <TextField variant="outlined" required label="State"/>
+                                <TextField variant="outlined" required label="Country"/>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} xl={6}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h4" component="h2" gutterBottom>3. Invoice</Typography>
+                            <Stack spacing={2}>
+                                <Grid container>
+                                    <Grid item xs={12} md={9}>
+                                        <TextField fullWidth variant="outlined" required label="Street"  />
+                                    </Grid>
+                                    <Grid item xs={12} md={3}>
+                                        <TextField fullWidth variant="outlined" required label="Housenumber"/>
+                                    </Grid>
+                                </Grid>
+                                <TextField variant="outlined" label="Floor" />
+                                <Grid container>
+                                    <Grid item xs={12} md={3}>
+                                        <TextField fullWidth variant="outlined" required label="Zipcode"  />
+                                    </Grid>
+                                    <Grid item xs={12} md={9}>
+                                        <TextField fullWidth variant="outlined" required label="City"/>
+                                    </Grid>
+                                </Grid>
+                                <TextField variant="outlined" required label="State"/>
+                                <TextField variant="outlined" required label="Country"/>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} xl={6}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h4" component="h2" gutterBottom>4. Customer Group</Typography>
+                            <Stack spacing={2}>
+                                <TextField variant="outlined" required label="Customer Group"/>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} xl={6}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h4" component="h2" gutterBottom>5. Contacts</Typography>
+
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
     );
 }
 
