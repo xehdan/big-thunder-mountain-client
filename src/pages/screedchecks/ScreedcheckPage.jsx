ea98036b5663e200bba83422450467d40e738f1a
xehdan <xehdan@yahoo.de>
2023-01-23 14:02:33 +0100

    Updated license for ckeditor.
    Created ScreedcheckProtocol.jsx
    Updated ScreedcheckPage.jsx to make protocols available for download

--- src/pages/screedchecks/ScreedcheckPage.jsx
+++ src/pages/screedchecks/ScreedcheckPage.jsx
@@ -1,39 +1,76 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import http from "../../http";
-import {Card, CardContent, Grid} from "@mui/material";
+import {Button, Card, CardContent, Grid} from "@mui/material";
+import {PDFDownloadLink} from "@react-pdf/renderer";
+import ComplaintProtocol from "../../components/pdfGen/ComplaintProtocol";
+import {Print} from "@mui/icons-material";
+import screedcheckProtocol from "../../components/pdfGen/ScreedcheckProtocol";
+import {UserContext} from "../../context/UserContext";
+import ScreedcheckProtocol from "../../components/pdfGen/ScreedcheckProtocol";
+import {Skeleton} from "@mui/lab";
+import pjson from "../../../package.json";
 
 const ScreedcheckPage = () => {
     let {id} = useParams();
     const [screedcheck, setScreedcheck] = useState([])
     const [customer, setCustomer] = useState([])
+    const user = useContext(UserContext)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const readScreedcheck = async () => {
-            const response = await http.get(`/api/screedcheck/${id}`, {
+            setLoading(true)
+            await http.get(`/api/screedcheck/${id}`, {
                 params: {
                     detail: "true"
                 }
-            });
-            setScreedcheck(response.data.screedcheck)
+            })
+                .then((response) => {
+                    setScreedcheck(response.data.screedcheck)
+                    const customerId = response.data.screedcheck.Project.customerId
+                    http.get(`/api/customer/${customerId}`, {
+                        params: {
+                            detail: "true"
+                        }
+                    })
+                        .then((response2) => {
+                            setCustomer(response2.data.customer);
+                        })
+                        .then(() => {
+                            setLoading(false)
+                        })
+                })
+
+
         }
         return readScreedcheck
     }, [id])
 
-    useEffect(() => {
-        const readCustomer = async () => {
-            const response = await http.get(`/api/customer/${id}`, {
-                params: {
-                    detail: "true"
-                }
-            });
-            setCustomer(response.data.customer);
-        };
-        return readCustomer
-    }, [id])
+
+    const LinkBehavior = React.forwardRef((props, ref) => (
+        <PDFDownloadLink
+            document={
+                <ScreedcheckProtocol
+                    title={`Screedcheck Checklist - ${customer.lastName ? customer.lastName + ', ' + customer.firstName : ''} ${customer.company ? customer.company : ''}`}
+                    author={user.firstName + ' ' + user.lastName}
+                    subject={`Screedcheck `}
+                    keywords="tbf"
+                    creator={`${pjson.name}-${pjson.version}`}
+                    screedcheck={screedcheck}
+
+                />}
+            fileName={`${screedcheck.transactionId.slice(-12)} - Screedcheck`}
+            ref={ref}
+            to="/"
+            {...props}
+            role={undefined}
+        />
+    ));
+
 
     return (
-        <Grid container spacing={2} sx={{ margin: 3}}>
+        <Grid container spacing={2} sx={{margin: 3}}>
             <Grid item xl={3} xs={4}>
                 <Card>
                     <CardContent>
@@ -43,6 +80,16 @@ const ScreedcheckPage = () => {
                 <Card>
                     <CardContent>
                         {JSON.stringify(customer)}
+                    </CardContent>
+                </Card>
+            </Grid>
+
+            <Grid item xl={3} xs={12}>
+                <Card>
+                    <CardContent>
+                        {loading ? <Skeleton/> : <Button startIcon={<Print/>} component={LinkBehavior} variant="outlined">
+                            Print Protocol
+                        </Button> }
                     </CardContent>
                 </Card>
             </Grid>
